54fc2de264797fe76cdd056a40c4562780be3063
Dylan Graham <dylan.graham2000@outlook.com>
2019-06-02 20:58:43 +1000

    Porting

--- moxane client/source/app.d
+++ moxane client/source/app.d
@@ -1,7 +1,61 @@
 import std.stdio;
 
+import moxane.core;
+import moxane.graphics.renderer;
+import moxane.graphics.sprite;
+import moxane.io;
+import moxane.ui;
+import squareone.scenes.splash;
+
+import dlib.math;
+
 void main()
 {
-	writeln("Edit source/app.d to start your project.");
-	while(true) {}
+	const MoxaneBootSettings settings = 
+	{
+		logSystem : true,
+		windowSystem : true,
+		graphicsSystem : true,
+		assetSystem : true,
+		physicsSystem : false,
+		networkSystem : false,
+		settingsSystem : false,
+		asyncSystem : true,
+		entitySystem : true,
+		sceneSystem : true
+	};
+	Moxane moxane = new Moxane(settings, "Square One");
+
+	Window win = moxane.services.get!Window;
+	Renderer r = moxane.services.get!Renderer;
+	EntityManager entityManager = moxane.services.get!EntityManager;
+	SceneManager sceneManager = moxane.services.get!SceneManager;
+	moxane.services.register!SpriteRenderer(new SpriteRenderer(moxane, r));
+	r.uiRenderables ~= moxane.services.get!SpriteRenderer;
+
+	r.primaryCamera.perspective.fieldOfView = 90f;
+	r.primaryCamera.perspective.near = 0.1f;
+	r.primaryCamera.perspective.far = 10f;
+	r.primaryCamera.isOrtho = false;
+	r.primaryCamera.position = Vector3f(0f, 0f, 0f);
+	r.primaryCamera.rotation = Vector3f(0f, 0f, 0f);
+	r.primaryCamera.buildView;
+	r.primaryCamera.buildProjection;
+
+	win.onFramebufferResize.add((win, size) @trusted {
+		r.primaryCamera.width = size.x;
+		r.primaryCamera.height = size.y;
+		r.primaryCamera.buildProjection;
+		r.uiCamera.width = size.x;
+		r.uiCamera.height = size.y;
+		r.uiCamera.deduceOrtho;
+		r.uiCamera.buildProjection;
+		r.cameraUpdated;
+	});
+
+	entityManager.add(new UISystem(moxane, entityManager));
+
+	sceneManager.current = new SplashscreenScene(moxane, null);
+
+	moxane.run;
 }
--- moxane client/source/squareone/scenes/splash.d
+++ moxane client/source/squareone/scenes/splash.d
@@ -1,8 +1,10 @@
 module squareone.scenes.splash;
 
 import moxane.core;
+import moxane.graphics.transformation;
+import moxane.ui;
 
-import moxane.ui.ecs;
+import dlib.math;
 
 final class SplashscreenScene : Scene
 {
@@ -13,8 +15,17 @@ final class SplashscreenScene : Scene
 		super(moxane, parent);
 
 		testButton = new Entity;
+		moxane.services.get!EntityManager().add(testButton);
+		Transform* t = testButton.createComponent!Transform;
+		*t = Transform.init;
+		t.position.x = 20f;
+		t.position.y = 20f;
 		UIButton* b = testButton.createComponent!UIButton;
-		
+		b.offset = Vector2i(0, 0);
+		b.dimensions = Vector2i(100, 20);
+		b.inactiveColour = Vector4f(1f, 0.5f, 0.1f, 1f);
+		b.hoverColour = Vector4f(0f, 1f, 0.1f, 1f);
+		b.clickColour = Vector4f(0f, 0f, 1f, 1f);
 	}
 
 	override void setToCurrent(Scene overwrote)
